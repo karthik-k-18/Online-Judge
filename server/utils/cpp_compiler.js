434ad812cf418fdd830a1fb817f2132154849df4
karthik-k-18 <200010024@iitdh.ac.in>
2023-07-25 17:27:15 +0530

    excecuting user code on docker

--- server/utils/cpp_compiler.js
+++ server/utils/cpp_compiler.js
@@ -16,10 +16,18 @@ const compile = async (inputfilepath, executablefilepath) => {
 
 const run = async (executablefilepath, input, output) => {
   return new Promise((resolve, reject) => {
-    const child = spawn(executablefilepath);
-    child.stdin.write(input);
-    child.stdin.end();
-    child.stdout.on("data", (data) => {
+    const process = spawn("docker", [
+      "run",
+      "--rm", // Remove container after exit.
+      "-i",
+      "test",
+      "sh", // Use shell inside the container to execute the command.
+      "-c",
+      executablefilepath, // Pass the command to be executed as an argument.
+    ]);
+    process.stdin.write(input);
+    process.stdin.end();
+    process.stdout.on("data", (data) => {
       data = data.toString().trim();
       if (data.toString() === output) {
         resolve("Accepted");
@@ -27,7 +35,7 @@ const run = async (executablefilepath, input, output) => {
         resolve("Wrong Answer");
       }
     });
-    child.stderr.on("data", (data) => {
+    process.stderr.on("data", (data) => {
       reject("Runtime error!");
     });
   });
@@ -36,3 +44,30 @@ const run = async (executablefilepath, input, output) => {
 
 
 export { compile, run };
+
+
+// const run = async (executablefilepath, input, output) => {
+//   return new Promise((resolve, reject) => {
+//     const process = spawn("docker", [
+//       "run",
+//       // "--rm", // remove container after exit.
+//       "-i",
+//       "test",
+//     ]);
+//     const child = spawn(executablefilepath);
+//     child.stdin.write(input);
+//     child.stdin.end();
+//     child.stdout.on("data", (data) => {
+//       data = data.toString().trim();
+//       if (data.toString() === output) {
+//         resolve("Accepted");
+//       } else {
+//         resolve("Wrong Answer");
+//       }
+//     });
+//     child.stderr.on("data", (data) => {
+//       reject("Runtime error!");
+//     });
+//   });
+// };
+
