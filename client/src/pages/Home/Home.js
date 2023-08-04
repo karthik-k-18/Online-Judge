a0451e9b45dc9b3b22cb0583b0f3e3959e2b50ad
karthik-k-18 <200010024@iitdh.ac.in>
2023-08-04 11:52:08 +0530

    imported monaco editor

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,10 +1,8 @@
 
-
+import MonacoEditor from "../../components/Editor/Editor";
 function Home(){
     return(
-        <div>
-        <h1>Home</h1>
-        </div>
+        <MonacoEditor />
     );
 }
 
