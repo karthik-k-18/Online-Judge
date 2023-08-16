3270dc8e353d4e2b965346a9e8083c7291ea7493
karthik-k-18 <200010024@iitdh.ac.in>
2023-08-16 22:23:00 +0530

    giving status of submitted problems for authenticated users

--- server/controllers/ProblemController.js
+++ server/controllers/ProblemController.js
@@ -2,32 +2,41 @@ import fs from "fs-extra";
 import { v4 as uuidv4 } from "uuid";
 import path from "path";
 
-import { Problem, TestCase, Submission,User} from "../models/models.js";
+import { Problem, TestCase, Submission, User } from "../models/models.js";
 import { compile, run } from "../utils/cpp_compiler.js";
 
 //@desc    Get all problems
 const getProblems = async (req, res) => {
   try {
+    // console.log(req.user);
     const problems = await Problem.find({});
     problems.sort((a, b) => a.id - b.id);
     if (req.user === undefined) {
       return res.status(200).json(problems);
     } else {
-      const submissions = req.user.submissions;
-      console.log(submissions);
-      if (submissions) {
+      const user = await User.findOne({ email: req.user });
+      const allSubmissions = await Submission.find({}); //all submissions by all users
+      const submissions = user.submissions;
+      // console.log(submissions);
+      if (submissions.length > 0) {
+        let updatedProblems = [];
         problems.forEach((problem) => {
-          //check if the user has submitted the problem and has "AC" verdict
-          const submission = submissions.find(
-            (submission) =>
-              submission.problem == problem._id && submission.verdict == "AC"
-          );
-          if (submission) {
-            problem.status = "AC";
-          } else {
-            problem.status = "NA";
+          // console.log("problem",problem.id,problem._id);
+          let flag=false;
+          for(let i=0;i<submissions.length;i++){
+            let userSubmission= allSubmissions.find((submission)=>submission._id.toString()===submissions[i].toString());
+            if(userSubmission.problem.toString()===problem._id.toString() && userSubmission.verdict=='AC'){
+              updatedProblems.push({...problem._doc,status:"AC"});
+              flag=true;
+              break;
+            }
+          }
+          if(!flag){
+            updatedProblems.push({...problem._doc,status:"NA"});
           }
         });
+        // console.log(updatedProblems);
+        return res.status(200).json(updatedProblems);
       }
       return res.status(200).json(problems);
     }
@@ -61,46 +70,44 @@ const submitProblem = async (req, res) => {
     );
     await fs.writeFile(inputFilepath, code);
     let verdict = undefined;
-    compile(inputFilepath, executableFilepath)
-      .then((message) => {
-        run(executableFilepath, testCase.input, testCase.output)
-          .then((result) => {
-            if (result === "AC") {
-              verdict = "AC";
-              res.status(200).json({ message: "Accepted!" });
-            } else {
-              verdict = "WA";
-              res.status(200).json({ message: "Wrong answer!" });
-            }
-          })
-          .catch((err) => {
-            verdict = "RE";
-            res.status(404).json({ message: "Runtime error!" });
-          });
-      })
-      .catch((err) => {
-        verdict = "CE";
-        res.status(404).json({ message: "Compilation error!" });
-      })
-      .finally(async () => {
-        // fs.unlink(inputFilepath);
-        // fs.unlink(executableFilepath);
-        const submission = new Submission({
-          problem: problem._id,
-          verdict: verdict,
-          submission_time: Date.now(),
-          code: code,
-        });
-        submission.save();
-        //add the submission to the user
-        const user = await User.findOne({ email: req.user });
-        // console.log(req.user._id)
-        console.log(user);
-        if(user.submissions===undefined) user.submissions=[];
-        user.submissions.push(submission._id);
-        user.save();
+    try {
+      console.log("compiling");
+      await compile(inputFilepath, executableFilepath);
+      console.log("compiled");
+      try{
+        const result = await run(executableFilepath,testCase.input,testCase.output);
+        if (result === "AC") {
+          verdict = "AC";
+          res.status(200).json({ message: "Accepted!" });
+        } else {
+          verdict = "WA";
+          res.status(200).json({message: "Wrong answer!"});
+        }
+      }catch(err){
+        verdict = "RE";
+        res.status(404).json({ message: "Runtime error!" });
+      }
+    } catch (err) {
+      verdict = "CE";
+      res.status(404).json({ message: "Compilation error!" });
+    } finally {
+      // fs.unlink(inputFilepath);
+      // fs.unlink(executableFilepath);
+      console.log(verdict);
+      const submission = new Submission({
+        problem: problem._id,
+        verdict: verdict,
+        submission_time: Date.now(),
+        code: code,
       });
+      submission.save();
 
+      //add the submission to the user
+      const user = await User.findOne({ email: req.user });
+      if (user.submissions === undefined) user.submissions = [];
+      user.submissions.push(submission._id);
+      user.save();
+    }
     //delete the files
   } catch (err) {
     res.status(404).json({ message: "Problem not found!" });
--- server/middlewares/authenticate.js
+++ server/middlewares/authenticate.js
@@ -9,8 +9,7 @@ export const authenticateUser = (req, res, next) => {
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
       if (err) return res.sendStatus(403); 
       // console.log(user)
-      req.user = user.name; 
-      // console.log(req.user)
+      req.user = user.email; 
       next(); 
     });
   } else {
@@ -18,8 +17,8 @@ export const authenticateUser = (req, res, next) => {
     const token = authHeader && authHeader.split(" ")[1]; // get the token from the authorization header
     if (token == null) return next(); // if there isn't any token, return 401
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-      if (err) return next(); // if the token is invalid, return 403
-      req.user = user; // store the user data in the request object
+      if (err) return next(); 
+      req.user = user.email; // store the user data in the request object
       next(); // pass the execution off to whatever request the client intended
     });
   }
